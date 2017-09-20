1540baf3ee953b4741279f88b20bf814e2514d4f
cohringer <caseyro@terpmail.umd.edu>
2017-09-19 21:54:05 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,35 @@
+
+def subtract( a, b ):
+	c=a-b
+	pass c
+
+def hierarchy( score ):
+	if score>=0 && score<131
+		print("This student has a score of "+score+". They are a Guinea Pig.")
+	elif score>=131 && score<216
+		print("This student has a score of "+score+". They are a Perceptive Noodle.")
+	elif score>=216 && score<351 
+		print("This student has a score of "+score+". They are a Nameless Apprentice")
+	elif score>=351 && score<426
+		print("This student has a score of "+score+". They are an Agent of Education.")
+	elif score>= 426
+		print("This student has a score of "+score+". They are a Primary Contributer.")
+	pass
+
+def sum0( myList ):
+	c=0
+	for i in range len(myList)
+		c+=myList[i]
+	pass c
+
+def sum1( myMatrix ):
+	# Your Implementation Here
+	pass
+
+def sum2( myMatrix ):
+	# Your Implementation Here
+	pass
+
+def moonTime( year, month, day ):
+	# Your Implementation Here
+	pass
